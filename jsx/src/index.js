3431762b44375421dc786025e50dc1fd75c6ecfc
An (Martin) Luo <aluo1.developer@gmail.com>
2020-05-31 11:39:18 +0800

    update the index

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,7 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a React component.
-const App = () => <div>Hi there!</div>;
+const App = () => (
+  <div>
+    <label className={"label"} htmlFor={"name"}>
+      Enter name:{" "}
+    </label>
+    <input id={"name"} type={"text"} />
+    <button style={{ backgroundColor: "blue", color: "white" }}>Submit</button>
+  </div>
+);
 
 // Take the React component and show it on the screen.
 ReactDOM.render(<App />, document.querySelector("#root"));
