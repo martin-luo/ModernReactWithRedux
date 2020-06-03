9d60082c900c0503969ac5402f7f68d5d38d6f77
An (Martin) Luo <aluo1.developer@gmail.com>
2020-06-03 15:38:12 +0800

    remove generated files and install dependencies

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import React and ReactDOM libraries.
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
+import { App } from "./components/App";
+import reducers from "./reducers";
+
+// Take the React component and show it on the screen.
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers, applyMiddleware(reduxThunk))}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.querySelector("#root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
