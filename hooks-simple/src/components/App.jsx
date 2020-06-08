bc70b399720003eca0bfff4f55775e39f2aac3ff
An (Martin) Luo <aluo1.developer@gmail.com>
2020-06-08 14:45:46 +0800

    reuse useResource in UserList

--- hooks-simple/src/components/App.jsx
+++ hooks-simple/src/components/App.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { ResourceList } from "./ResourceList";
+import { UserList } from "./UserList";
 
 const App = () => {
   const [resource, setResource] = useState("posts");
 
   return (
     <div className={"ui container"}>
+      <UserList />
       <div>
         <button className={"ui button"} onClick={() => setResource("posts")}>
           Posts
