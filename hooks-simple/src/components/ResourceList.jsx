e79296e85dd98dd20d1f0f4eefe0841626bf64b1
An (Martin) Luo <aluo1.developer@gmail.com>
2020-06-08 14:40:45 +0800

    extract the hooks and make it reusable

--- hooks-simple/src/components/ResourceList.jsx
+++ hooks-simple/src/components/ResourceList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { resourceAPI } from "../apis/resource";
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = async (resource) => {
@@ -22,6 +22,12 @@ const ResourceList = ({ resource }) => {
     //   setResources(response.data);
     // })(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
